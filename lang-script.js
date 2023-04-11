0503605f504a9977db2cdad41820a0953432f442
Viraj Lasitha <47885958+VirajLasitha@users.noreply.github.com>
2023-04-11 16:32:39 +0400

    .

--- lang-script.js
+++ lang-script.js
@@ -1,130 +1,132 @@
 const translations = {
   en: {
-    'companyName':'ACROSS Co. Ltd',
-    'companyLastName':'Co. Ltd',
+    'companyName': 'ACROSS Co. Ltd',
+    'companyLastName': 'Co. Ltd',
 
     'navHome': 'Home',
     'navSer': 'Services',
-    'navClient':'Our Clients',
-    'navWho':'Who We Are',
-    'navContact':'Contact',
+    'navClient': 'Our Clients',
+    'navWho': 'Who We Are',
+    'navContact': 'Contact',
     'hero-title': 'A IT Services & Software Development Company',
     'hero-desc': 'We provide innovative IT solutions for businesses with expertise in network system construction, software development, and IT product sales. Focused on quality and customer satisfaction.',
-    'hero-cta': 'Contact Us on LINE',
+    'hero-cta': 'Contact Us',
 
-    'serHeading':'How can we help your business?',
-    'serMainDesc':'From building robust network systems to developing custom software, we help businesses thrive with reliable IT solutions and expert support.',
-    'sercateHead1':'Network System Construction',
-    'sercateDesc1':'Building secure and reliable computer networks for businesses.',
-    'sercateHead2':'System Introduction and Operation Support',
-    'sercateDesc2':'Providing assistance and maintenance for new and existing systems.',
-    'sercateHead3':'Software Development',
-    'sercateDesc3':'Creating custom software solutions for specific business needs.',
-    'sercateHead4':'Sales of IT-Related Products',
-    'sercateDesc4':'Offering a range of products to meet technology needs.',
-    
-    'clientHeading':'Our Clients',
-    'clientDesc':'Several selected clients, who already believe in our service.',
+    'serHeading': 'How can we help your business?',
+    'serMainDesc': 'From building robust network systems to developing custom software, we help businesses thrive with reliable IT solutions and expert support.',
+    'sercateHead1': 'Network System Construction',
+    'sercateDesc1': 'Building secure and reliable computer networks for businesses.',
+    'sercateHead2': 'System Introduction and Operation Support',
+    'sercateDesc2': 'Providing assistance and maintenance for new and existing systems.',
+    'sercateHead3': 'Software Development',
+    'sercateDesc3': 'Creating custom software solutions for specific business needs.',
+    'sercateHead4': 'Sales of IT-Related Products',
+    'sercateDesc4': 'Offering a range of products to meet technology needs.',
 
-    'whoHeading':'Who we are?',
-    'whoDesc':'We are a versatile IT company specializing in network system construction, software development, and sales of IT-related products. With years of experience, we provide top-notch system introduction and operation support to our clients. Our team of experts is dedicated to delivering innovative solutions that meet our clients\' specific needs. Whether it\'s building a robust network system, developing custom software, or providing IT product sales, we are committed to delivering exceptional services that exceed our clients\' expectations.',
-    
-    'contactHeading':'Contact Us',
-    'contactDesc':'Want to get in touch? We\'d love to hear from you. Here\'s how you can reach us.',
+    'clientHeading': 'Our Clients',
+    'clientDesc': 'Several selected clients, who already believe in our service.',
 
-    'footerAbout':'We provide innovative IT solutions for businesses with expertise in network system construction, software development, and IT product sales. Focused on quality and customer satisfaction.',
+    'whoHeading': 'Who we are?',
+    'whoDesc': 'We are a versatile IT company specializing in network system construction, software development, and sales of IT-related products. With years of experience, we provide top-notch system introduction and operation support to our clients. Our team of experts is dedicated to delivering innovative solutions that meet our clients\' specific needs. Whether it\'s building a robust network system, developing custom software, or providing IT product sales, we are committed to delivering exceptional services that exceed our clients\' expectations.',
+
+    'contactHeading': 'Contact Us',
+    'contactDesc': 'Want to get in touch? We\'d love to hear from you. Here\'s how you can reach us.',
+
+    'footerAbout': 'We provide innovative IT solutions for businesses with expertise in network system construction, software development, and IT product sales. Focused on quality and customer satisfaction.',
 
     'fcateheading1': 'Who we are',
     'fcateheading2': 'Company',
     'fcateheading3': 'Support',
     'fcateheading4': 'Contact Us',
 
-    'fwedo1':'Network System Construction',
-    'fwedo2':'Operation Support',
-    'fwedo3':'Software Development',
-    'fwedo4':'IT-Related Products',
+    'fwedo1': 'Network System Construction',
+    'fwedo2': 'Operation Support',
+    'fwedo3': 'Software Development',
+    'fwedo4': 'IT-Related Products',
 
-    'fcomoant1':'About Us',
-    'fcomoant2':'Career',
-    'fcomoant3':'Become Investor',
+    'fcomoant1': 'About Us',
+    'fcomoant2': 'Career',
+    'fcomoant3': 'Become Investor',
 
-    'fsupport1':'FAQ',
+    'fsupport1': 'FAQ',
 
-    'fcontact1':'WhatsApp',
-    'fcontact2':'Line',
-    'fcontact3':'Support 24/7',
+    'fcontact1': 'WhatsApp',
+    'fcontact2': 'Line',
+    'fcontact3': 'Support 24/7',
 
     'footerCopyright': 'Copyright',
     'footerAllRightReserved': 'All rights reserved.'
-    
+
   },
   fr: {
-    'companyName':'ACROSS 株式会社',
-    'companyLastName':'株式会社',
+    'companyName': 'ACROSS 株式会社',
+    'companyLastName': '株式会社',
 
     'navHome': ' ホーム',
     'navSer': 'サービス',
     'navClient': '当社のお客様',
-    'navWho':'当社について',
-    'navContact':'お問い合わせ',
+    'navWho': '当社について',
+    'navContact': 'お問い合わせ',
     'hero-title': 'ITサービス・ソフトウェア開発会社',
     'hero-desc': '私たちは、ネットワークシステム構築、ソフトウェア開発、IT製品販売の専門知識を持ち、ビジネス向けに革新的なITソリューションを提供しています。品質とお客様満足度に重点を置いています。',
-    'hero-cta': 'LINEでお問い合わせください',
-    
-    'serHeading':'お客様のビジネスにお役に立てることがありますか？',
-    'serMainDesc':'堅牢なネットワークシステムの構築からカスタムソフトウェアの開発まで、当社は信頼できるITソリューションと専門的なサポートでビジネスの発展を支援します。',
-    'sercateHead1':'ネットワークシステムの構築',
-    'sercateDesc1':'ビジネス向けに安全で信頼性の高いコンピューターネットワークを構築します。',
-    'sercateHead2':'システム導入と運用サポート',
-    'sercateDesc2':'新規および既存のシステムに対する支援とメンテナンスを提供します。',
-    'sercateHead3':'ソフトウェア開発',
-    'sercateDesc3':'特定のビジネスニーズに合わせたカスタムソフトウェアソリューションの開発を行います。',
-    'sercateHead4':'IT関連製品の販売',
-    'sercateDesc4':'さまざまな製品を提供して、技術ニーズに応えます。',
+    'hero-cta': 'お問い合わせ',
 
-    'clientHeading':'当社のお客様',
-    'clientDesc':'当社のサービスを信頼しているいくつかの選ばれたお客様です。',
+    'serHeading': 'お客様のビジネスにお役に立てることがありますか？',
+    'serMainDesc': '堅牢なネットワークシステムの構築からカスタムソフトウェアの開発まで、当社は信頼できるITソリューションと専門的なサポートでビジネスの発展を支援します。',
+    'sercateHead1': 'ネットワークシステムの構築',
+    'sercateDesc1': 'ビジネス向けに安全で信頼性の高いコンピューターネットワークを構築します。',
+    'sercateHead2': 'システム導入と運用サポート',
+    'sercateDesc2': '新規および既存のシステムに対する支援とメンテナンスを提供します。',
+    'sercateHead3': 'ソフトウェア開発',
+    'sercateDesc3': '特定のビジネスニーズに合わせたカスタムソフトウェアソリューションの開発を行います。',
+    'sercateHead4': 'IT関連製品の販売',
+    'sercateDesc4': 'さまざまな製品を提供して、技術ニーズに応えます。',
 
-    'whoHeading':'私たちは誰ですか',
-    'whoDesc':'当社は、ネットワークシステムの構築、ソフトウェア開発、IT関連製品の販売に特化した多才なIT企業です。数年にわたる経験を生かし、当社はお客様に優れたシステム導入と運用サポートを提供しています。専門家チームは、お客様の特定のニーズに応える革新的なソリューションを提供することに専念しています。堅牢なネットワークシステムの構築、カスタムソフトウェアの開発、またはIT製品の販売であれ、当社は、お客様の期待を超える優れたサービスを提供することに取り組んでいます。',
+    'clientHeading': '当社のお客様',
+    'clientDesc': '当社のサービスを信頼しているいくつかの選ばれたお客様です。',
 
-    'contactHeading':'お問い合わせ',
-    'contactDesc':'お問い合わせいただけますか？あなたからのご連絡をお待ちしております。以下の方法でお問い合わせください。',
+    'whoHeading': '私たちは誰ですか',
+    'whoDesc': '当社は、ネットワークシステムの構築、ソフトウェア開発、IT関連製品の販売に特化した多才なIT企業です。数年にわたる経験を生かし、当社はお客様に優れたシステム導入と運用サポートを提供しています。専門家チームは、お客様の特定のニーズに応える革新的なソリューションを提供することに専念しています。堅牢なネットワークシステムの構築、カスタムソフトウェアの開発、またはIT製品の販売であれ、当社は、お客様の期待を超える優れたサービスを提供することに取り組んでいます。',
 
-    'footerAbout':'当社は、ネットワークシステムの構築、ソフトウェア開発、IT製品販売において高い専門知識を持ち、革新的なITソリューションを提供しています。品質とお客様の満足度に注力し、ビジネス向けのITソリューションを提供しています。',
+    'contactHeading': 'お問い合わせ',
+    'contactDesc': 'お問い合わせいただけますか？あなたからのご連絡をお待ちしております。以下の方法でお問い合わせください。',
 
-    'fcateheading1':'私たちの仕事',
-    'fcateheading2':' 会社',
-    'fcateheading3':'サポート',
-    'fcateheading4':'お問い合わせ',
-    
+    'footerAbout': '当社は、ネットワークシステムの構築、ソフトウェア開発、IT製品販売において高い専門知識を持ち、革新的なITソリューションを提供しています。品質とお客様の満足度に注力し、ビジネス向けのITソリューションを提供しています。',
 
-    'fwedo1':'ネットワークシステムの構築',
-    'fwedo2':'運用サポート',
-    'fwedo3':'ソフトウェア開発',
-    'fwedo4':'IT関連製品',
+    'fcateheading1': '私たちの仕事',
+    'fcateheading2': ' 会社',
+    'fcateheading3': 'サポート',
+    'fcateheading4': 'お問い合わせ',
 
-    'fcomoant1':'会社概要',
-    'fcomoant2':'キャリア',
-    'fcomoant3':'投資家になる',
 
-    'fsupport1':'よくある質問',
+    'fwedo1': 'ネットワークシステムの構築',
+    'fwedo2': '運用サポート',
+    'fwedo3': 'ソフトウェア開発',
+    'fwedo4': 'IT関連製品',
 
-    'fcontact1':'WhatsApp',
-    'fcontact2':'Line',
-    'fcontact3':'ホットライン',
+    'fcomoant1': '会社概要',
+    'fcomoant2': 'キャリア',
+    'fcomoant3': '投資家になる',
+
+    'fsupport1': 'よくある質問',
+
+    'fcontact1': 'WhatsApp',
+    'fcontact2': 'Line',
+    'fcontact3': 'ホットライン',
 
     'footerCopyright': '著作権',
     'footerAllRightReserved': '全著作権所有。',
   }
 };
 
-function translatePage(language) {
+function translatePage(language)
+{
   // Get all elements with a "data-translation-key" attribute
   const elements = document.querySelectorAll('[data-translation-key]');
-  
+
   // Loop through each element and set its text content to the corresponding translation
-  elements.forEach((element) => {
+  elements.forEach((element) =>
+  {
     const translationKey = element.getAttribute('data-translation-key');
     element.textContent = translations[language][translationKey];
   });
@@ -136,13 +138,16 @@ translatePage(language);
 
 // Add a click event listener to each language button
 const languageButtons = document.querySelectorAll('.language-button');
-languageButtons.forEach((button) => {
-  button.addEventListener('click', () => {
+languageButtons.forEach((button) =>
+{
+  button.addEventListener('click', () =>
+  {
     // Get the language from the button's data attribute
     const buttonLanguage = button.getAttribute('data-language');
-    
+
     // Only translate the page if the button's language is different from the current language
-    if (buttonLanguage !== language) {
+    if (buttonLanguage !== language)
+    {
       language = buttonLanguage;
       translatePage(language);
     }
@@ -150,19 +155,25 @@ languageButtons.forEach((button) => {
 });
 
 // Update active state of language buttons
-languageButtons.forEach(button => {
-  button.addEventListener('click', () => {
+languageButtons.forEach(button =>
+{
+  button.addEventListener('click', () =>
+  {
     setActiveLanguageButton(button);
     translatePage(button.dataset.language);
   });
 });
 
-function setActiveLanguageButton(activeButton) {
+function setActiveLanguageButton(activeButton)
+{
   const languageButtons = document.querySelectorAll('.language-button');
-  languageButtons.forEach(button => {
-    if (button === activeButton) {
+  languageButtons.forEach(button =>
+  {
+    if (button === activeButton)
+    {
       button.classList.add('active');
-    } else {
+    } else
+    {
       button.classList.remove('active');
     }
   });
