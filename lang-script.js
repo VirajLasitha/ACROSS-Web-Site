7b87f6279b5a02a91fd8ce19abb6c93e9ae36952
Viraj Abeyrathna <virajabeyrathna@gmail.com>
2024-04-05 14:35:59 +0900

    .

--- lang-script.js
+++ lang-script.js
@@ -3,6 +3,9 @@ const translations = {
     'companyName': 'ACROSS Co. Ltd',
     'companyLastName': 'Co. Ltd',
 
+    'addressLine1': '〒103-0023',
+    'addressLine2': '3F Plaza Building, 2-6-1 Nihonbashi Honmachi, Chuo-ku, Tokyo',
+
     'navHome': 'Home',
     'navSer': 'Services',
     'navClient': 'Our Clients',
@@ -11,15 +14,15 @@ const translations = {
     'hero-title': 'A IT Services & Software Development Company',
     'hero-desc': 'We provide innovative IT solutions for businesses with expertise in network system construction, software development, and IT product sales. Focused on quality and customer satisfaction.',
     'hero-cta': 'Contact Us',
-    'sw-hero-title':'Software Development',
-    'sw-hero-desc':'Our team, featuring talent from Japan, China and Sri Lanka, offers tailor-made software solutions with a unique cultural perspective. Fluent in Japanese, Chinese, and English. We communicate effortlessly to bring your vision to life.',
+    'sw-hero-title': 'Software Development',
+    'sw-hero-desc': 'Our team, featuring talent from Japan, China and Sri Lanka, offers tailor-made software solutions with a unique cultural perspective. Fluent in Japanese, Chinese, and English. We communicate effortlessly to bring your vision to life.',
 
     'serHeading': 'How can we help your business?',
     'serMainDesc': 'From building robust network systems to developing custom software, we help businesses thrive with reliable IT solutions and expert support.',
     'sw-serHeading': 'Our Technology',
     'sw-serMainDesc': 'We specialize in .Net, Java, PHP, WordPress, Angular, MySQL, and more. Feel free to reach out to us for further information.',
-    
-    
+
+
     'sercateHead1': 'Network System Construction',
     'sercateHead2': 'System Introduction and Operation Support',
     'sercateDesc2': 'Providing assistance and maintenance for new and existing systems.',
@@ -35,6 +38,11 @@ const translations = {
 
     'whoHeading': 'Who we are?',
     'whoDesc': 'We are a versatile IT company specializing in network system construction, software development, and sales of IT-related products. With years of experience, we provide top-notch system introduction and operation support to our clients. Our team of experts is dedicated to delivering innovative solutions that meet our clients\' specific needs. Whether it\'s building a robust network system, developing custom software, or providing IT product sales, we are committed to delivering exceptional services that exceed our clients\' expectations.',
+
+    'news1': '2020 Apr 01　Company moved to Nihonbashi.',
+    'news2': '2017 Feb 01　Company moved to Roppongi.',
+    'news3': '2016 Dec 13　Website has been renewed.',
+    'news4': '2010 Apr 01　Company established',
 
     'contactHeading': 'Contact Us',
     'contactDesc': 'Want to get in touch? We\'d love to hear from you. Here\'s how you can reach us.',
@@ -52,7 +60,7 @@ const translations = {
     'fwedo4': 'IT-Related Products',
 
     'fcomoant1': 'About Us',
-    'fcomoant2': 'Career',
+    'fcomoant2': 'Privacy Policy',
     'fcomoant3': 'Become Investor',
 
     'fsupport1': 'FAQ',
@@ -63,12 +71,44 @@ const translations = {
     'fcontactus': 'Contact Us',
 
     'footerCopyright': 'Copyright',
-    'footerAllRightReserved': 'Across Co., Ltd. All rights reserved.'
+    'footerAllRightReserved': 'Across Co., Ltd. All rights reserved.',
+
+    'privacyPolicy-1': 'has established the following personal information protection policy, established a personal information protection system, and thoroughly ensures that all employees are aware of the importance of personal information protection and take measures to protect it. We will promote the protection of personal information.',
+
+    'managementOfPersonalInformation': 'Management of Personal Information',
+    'privacyPolicy-2': 'Our company keeps customers personal information (referring to personal information as defined in the Act on the Protection of Personal Information, Act No. 57 of 2003; the same applies hereinafter) accurate and up-to-date, and In order to prevent unauthorized access, loss, damage, falsification, leakage, etc., we will take necessary measures such as maintaining security systems, improving management systems, and thoroughly training employees, implement safety measures, and strictly manage personal information. I will do it.',
+
+    'purposeOfUseOfPersonalInformation': 'Purpose of Use of Personal Information',
+    'privacyPolicy-3': 'The personal information we receive from our customers will be used to send e-mails and materials in order to contact us, provide business guidance, and answer your questions.',
+
+    'prohibitionOfDisclosure': 'Prohibition of Disclosure/Provision of Personal Information to Third Parties',
+    'privacyPolicy-4': 'We will appropriately manage the personal information entrusted to us by our customers and will not disclose personal information to third parties except in the following cases.',
+    'privacyPolicy-4-1': 'If the customer has consented',
+    'privacyPolicy-4-2': 'When disclosing personal information to a company outsourced by the Company in order to provide a service requested by the customer',
+    'privacyPolicy-4-3': 'When announcing that the information will be used jointly',
+    'privacyPolicy-4-4': 'When it is necessary to disclose based on laws and regulations',
+
+    'securityMeasures': 'Security Measures For Personal Information',
+    'privacyPolicy-5': 'If a customer requests disclosure, correction, addition, deletion, erasure, or suspension of use of their personal information, we will respond after confirming the identity of the customer.',
+
+    'inquiryAboutThePerson': 'Inquiry About the Person',
+    'privacyPolicy-6': 'If a customer requests disclosure, correction, addition, deletion, erasure, or suspension of use of their personal information, we will respond after confirming the identity of the customer.',
+
+    'complianceAndReview': 'Compliance and Review of Laws and Regulations',
+    'privacyPolicy-7': 'Our company will comply with Japanese laws and regulations and other norms applicable to the personal information it holds, and will review the contents of this policy from time to time and strive to improve it.',
+
+    'inquiry': 'Inquiry',
+    'privacyPolicy-8': 'For inquiries regarding our handling of personal information, please contact us below.',
+
+    'contactInfo': 'Overview of Our Company',
 
   },
   jp: {
     'companyName': 'ACROSS 株式会社',
     'companyLastName': '株式会社',
+
+    'addressLine1': '〒103-0023',
+    'addressLine2': '東京都中央区日本橋本町2-6-1プラザビル3F',
 
     'navHome': ' ホーム',
     'navSer': 'サービス',
@@ -79,13 +119,13 @@ const translations = {
     'hero-desc': '当社はネットワークシステム構築、ソフトウェア開発、IT製品販売の専門知識を持つ、常に革新的なITソリューションを提供しています。品質とお客様満足度ナンバー1を目指しています。',
     'hero-cta': 'お問い合わせ',
     'sw-hero-title': 'ソフトウェア開発',
-    'sw-hero-desc':'日本、中国、スリランカからの才能あるチームが、ユニークな文化的視点で特注ソフトウェアソリューションを提供しています。日本語、中国語、英語が流暢で、お客様のビジョンを具現化するためにスムーズにコミュニケーションを図ります。',
+    'sw-hero-desc': '日本、中国、スリランカからの才能あるチームが、ユニークな文化的視点で特注ソフトウェアソリューションを提供しています。日本語、中国語、英語が流暢で、お客様のビジョンを具現化するためにスムーズにコミュニケーションを図ります。',
 
     'serHeading': 'お客様のビジネスにお役に立てることがありますか？',
     'serMainDesc': '堅牢なネットワークシステムの構築からカスタムソフトウェアの開発まで、当社は信頼できるITソリューションと専門的なサポートでビジネスの発展を支援します。',
     'sw-serHeading': '私たちのテクノロジー',
     'sw-serMainDesc': '.Net、Java、PHP、WordPress、Angular、MySQLなどに特化しています。詳細についてはお問い合わせください。',
-    
+
     'sercateHead1': 'ネットワークシステムの構築',
     'sercateDesc1': 'ビジネス向けに安全で信頼性の高いコンピューターネットワークを構築。',
     'sercateHead2': 'システム導入と運用サポート',
@@ -100,6 +140,11 @@ const translations = {
 
     'whoHeading': '弊社について',
     'whoDesc': '当社は、ネットワークシステムの構築、ソフトウェア開発、IT関連製品の販売に特化したIT企業です。長年にわたる経験を活かし、当社はお客様に満足のいくシステム導入と運用サポートを提供しています。当社のチームは、お客様のニーズに応える革新的なソリューションを提供しています。堅牢なネットワークシステムの構築、カスタムソフトウェアの開発、またはIT製品の販売など、当社はお客様の期待を超える優れたサービスを提供することに取り組んでいます。',
+
+    'news1': '2020年04月01日　弊社が日本橋に引越しました。',
+    'news2': '2017年02月01日　弊社が六本木に引越しました。',
+    'news3': '2016年12月13日　 弊社のホームページをリニューアルしました。',
+    'news4': '2010年04月01日　会社設立',
 
     'contactHeading': 'お問い合わせ',
     'contactDesc': 'お問い合わせ先は以下の方法でお願いします。',
@@ -118,7 +163,7 @@ const translations = {
     'fwedo4': 'IT関連製品',
 
     'fcomoant1': '会社概要',
-    'fcomoant2': 'キャリア',
+    'fcomoant2': 'プライバシーポリシー',
     'fcomoant3': '投資家になる',
 
     'fsupport1': 'よくある質問',
@@ -129,18 +174,47 @@ const translations = {
     'fcontactus': 'お問い合わせ',
 
     'footerCopyright': '著作権',
-    'footerAllRightReserved': 'Across Co., Ltd. 全著作権所有。',
+    'footerAllRightReserved': 'Across 株式会社 全著作権所有。',
+    // 'footerAllRightReserved': 'Across Co., Ltd. 全著作権所有。',
+
+    'privacyPolicy-1': 'は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みを構築し、全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、個人情報の保護を推進致します。',
+
+    'managementOfPersonalInformation': '個人情報の管理',
+    'privacyPolicy-2': '当社は、お客さまの個人情報（個人情報の保護に関する法律平成十五年法律第五十七号にいう個人情報を指します。以下同じ。）を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます',
+
+    'purposeOfUseOfPersonalInformation': '個人情報の利用目的',
+    'privacyPolicy-3': 'お客さまからお預かりした個人情報は、当社からのご連絡や業務のご案内やご質問に対する回答として、電子メールや資料のご送付に利用いたします。',
+
+    'prohibitionOfDisclosure': '個人情報の第三者への開示・提供の禁止',
+    'privacyPolicy-4': '当社は、お客さまよりお預かりした個人情報を適切に管理し、次のいずれかに該当する場合を除き、個人情報を第三者に開示いたしません。',
+    'privacyPolicy-4-1': 'お客さまの同意がある場合',
+    'privacyPolicy-4-2': 'お客さまが希望されるサービスを行なうために当社が業務を委託する業者に対して開示する場合',
+    'privacyPolicy-4-3': '共同利用する旨公表した場合',
+    'privacyPolicy-4-4': '法令に基づき開示することが必要である場合',
+
+    'securityMeasures': '個人情報の安全対策',
+    'privacyPolicy-5': '当社は、個人情報の正確性及び安全性確保のために、適切な組織的安全管理措置、人的安全管理措置、物理的安全管理措置、技術的安全管理措置を実施し、セキュリティに万全の対策を講じています。また、適宜、外的環境の把握にも努めています。',
+
+    'inquiryAboutThePerson': 'ご本人の照会',
+    'privacyPolicy-6': 'お客さまがご本人の個人情報の開示・訂正・追加・削除・消去または利用停止などをご希望される場合には、ご本人であることを確認の上、対応させていただきます。',
+
+    'complianceAndReview': '法令、規範の遵守と見直し',
+    'privacyPolicy-7': '当社は、保有する個人情報に関して適用される日本の法令、その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。',
+
+    'inquiry': 'お問い合わせ',
+    'privacyPolicy-8': '当社の個人情報の取扱に関するお問い合わせは下記までご連絡ください。',
+
+    'contactInfo': '当社の概要',
+
   }
 };
 
-function translatePage(language)
-{
+function translatePage(language) {
   // Get all elements with a "data-translation-key" attribute
   const elements = document.querySelectorAll('[data-translation-key]');
 
   // Loop through each element and set its text content to the corresponding translation
-  elements.forEach((element) =>
-  {
+  elements.forEach((element) => {
     const translationKey = element.getAttribute('data-translation-key');
     element.textContent = translations[language][translationKey];
   });
@@ -152,16 +226,13 @@ translatePage(language);
 
 // Add a click event listener to each language button
 const languageButtons = document.querySelectorAll('.language-button');
-languageButtons.forEach((button) =>
-{
-  button.addEventListener('click', () =>
-  {
+languageButtons.forEach((button) => {
+  button.addEventListener('click', () => {
     // Get the language from the button's data attribute
     const buttonLanguage = button.getAttribute('data-language');
 
     // Only translate the page if the button's language is different from the current language
-    if (buttonLanguage !== language)
-    {
+    if (buttonLanguage !== language) {
       language = buttonLanguage;
       translatePage(language);
     }
@@ -169,25 +240,19 @@ languageButtons.forEach((button) =>
 });
 
 // Update active state of language buttons
-languageButtons.forEach(button =>
-{
-  button.addEventListener('click', () =>
-  {
+languageButtons.forEach(button => {
+  button.addEventListener('click', () => {
     setActiveLanguageButton(button);
     translatePage(button.dataset.language);
   });
 });
 
-function setActiveLanguageButton(activeButton)
-{
+function setActiveLanguageButton(activeButton) {
   const languageButtons = document.querySelectorAll('.language-button');
-  languageButtons.forEach(button =>
-  {
-    if (button === activeButton)
-    {
+  languageButtons.forEach(button => {
+    if (button === activeButton) {
       button.classList.add('active');
-    } else
-    {
+    } else {
       button.classList.remove('active');
     }
   });
